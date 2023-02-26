4e47bb64499a3694fc282a8fda730b7350cf26e4
MMosquito <melissa.mosquito@gmail.com>
2023-02-26 16:37:55 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,7 @@ function buildTable(data) {
 
     data.forEach((dataRow) => {
        
-// Append a row to the tble body
+// Append a row to the table body
         
         let row = tbody.append("tr");
 
@@ -28,31 +28,31 @@ function buildTable(data) {
         );
       });
 
-    }
+    
 
-    function handleClick() {
+function handleClick() {
         
-        // Grab the datetime value from the filter
-        let date = d3.select("#datetime").property("value");
-        let filteredData = tableData;
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
       
-         // Check to see if a date was entered and filter the
-        // data using that date.
-        if (date) {
+  // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
         
-            // Apply `filter` to the table data to only keep the
-          // rows where the `datetime` value matches the filter value
-          filteredData = filteredData.filter(row => row.datetime === date);
-        }
+  // Apply `filter` to the table data to only keep the
+  // rows where the `datetime` value matches the filter value
+  filteredData = filteredData.filter(row => row.datetime === date);
+  };
       
-         // Rebuild the table using the filtered data
-        // @NOTE: If no date was entered, then filteredData will
-        // just be the original tableData.
-        buildTable(filteredData);
-      }
+  // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+};
       
-      // Attach an event to listen for the form button
-      d3.selectAll("#filter-btn").on("click", handleClick);
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
       
-      // Build the table when the page loads
-      buildTable(tableData);
+// Build the table when the page loads
+buildTable(tableData);
