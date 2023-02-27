2256c465e788fd7d40c898430316d1734b302ca0
MMosquito <melissa.mosquito@gmail.com>
2023-02-26 20:33:00 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -2,32 +2,32 @@
 const tableData = data;
 
 // Reference the HTML table using d3
-var today = d3.select("tbody")
+var tbody = d3.select("tbody")
 
 function buildTable(data) {
 // First, clear out any existing data
    
-    tbody.html("");
+  tbody.html("");
 
 // Next, loop throught each object in the data
 // and append a row and cells for each value in the row
 
-    data.forEach((dataRow) => {
+  data.forEach((dataRow) => {
        
 // Append a row to the table body
         
-        let row = tbody.append("tr");
+    let row = tbody.append("tr");
 
 //Loop through each field in the dataRow and add
 //each value as a table cell (td)
 
-        Object.values(dataRow).forEach((val) => {
-          let cell = row.append("td");
-          cell.text(val);
-          }
-        );
-      });
-
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+}
     
 
 function handleClick() {
@@ -42,7 +42,7 @@ function handleClick() {
         
   // Apply `filter` to the table data to only keep the
   // rows where the `datetime` value matches the filter value
-  filteredData = filteredData.filter(row => row.datetime === date);
+    filteredData = filteredData.filter(row => row.datetime === date);
   };
       
   // Rebuild the table using the filtered data
